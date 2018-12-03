1e0ba1398a173002ddb0d16ec496e252f3334296
bbarres <benoit.barres@gmail.com>
2018-12-03 14:17:27 +0100

    adding more graph for other active substance

--- mapevol.R
+++ mapevol.R
@@ -41,10 +41,14 @@ coorddep<-cbind("dep_ID"=ind_list,coorddep)
 ###############################################################################
 
 #producing the map
-QoIdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,ven_germ$QoI,exclude="")),
-               "Resistant"=table(ven_germ$dptmt,ven_germ$QoI,exclude="")[,1],
-               "Sensible"=table(ven_germ$dptmt,ven_germ$QoI,exclude="")[,2],
-               "Total"=rowSums(table(ven_germ$dptmt,ven_germ$QoI,exclude="")))
+QoIdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,
+                                        ven_germ$QoI,exclude="")),
+               "Resistant"=table(ven_germ$dptmt,
+                                 ven_germ$QoI,exclude="")[,1],
+               "Sensible"=table(ven_germ$dptmt,
+                                ven_germ$QoI,exclude="")[,2],
+               "Total"=rowSums(table(ven_germ$dptmt,
+                                     ven_germ$QoI,exclude="")))
 
 data2map<-merge(QoIdata,coorddep,by="dep_ID")
 
@@ -54,14 +58,16 @@ draw.pie(x=data2map$longitude,y=data2map$latitude,
          z=cbind((as.numeric(as.character(data2map$Resistant))),
                  (as.numeric(as.character(data2map$Sensible)))),
          col=c("red","blue"),
-         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),labels=NA)
+         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),
+         labels=NA)
 par(op)
 
 #producing the barplot
 datXyear<-t(prop.table(table(ven_germ$year,ven_germ$QoI,exclude=""),
                        margin=1)*100)
 totalyear<-rowSums(table(ven_germ$year,ven_germ$QoI,exclude=""))
-temp<-barplot(datXyear,col=c("red","blue"),las=1,font=2,main="QoI",cex.main=2)
+temp<-barplot(datXyear,col=c("red","blue"),las=1,font=2,main="QoI",
+              cex.main=2)
 text(temp[1],103,paste("n=",totalyear[1],sep=""),font=3,cex=1,xpd=TRUE)
 text(temp[2],103,paste("n=",totalyear[2],sep=""),font=3,cex=1,xpd=TRUE)
 text(temp[3],103,paste("n=",totalyear[3],sep=""),font=3,cex=1,xpd=TRUE)
@@ -80,10 +86,14 @@ text(temp[11],103,paste("n=",totalyear[11],sep=""),font=3,cex=1,xpd=TRUE)
 ###############################################################################
 
 #producing the map
-APdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,ven_germ$AP,exclude="")),
-               "Resistant"=table(ven_germ$dptmt,ven_germ$AP,exclude="")[,1],
-               "Sensible"=table(ven_germ$dptmt,ven_germ$AP,exclude="")[,2],
-               "Total"=rowSums(table(ven_germ$dptmt,ven_germ$AP,exclude="")))
+APdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,
+                                       ven_germ$AP,exclude="")),
+               "Resistant"=table(ven_germ$dptmt,
+                                 ven_germ$AP,exclude="")[,1],
+               "Sensible"=table(ven_germ$dptmt,
+                                ven_germ$AP,exclude="")[,2],
+               "Total"=rowSums(table(ven_germ$dptmt,
+                                     ven_germ$AP,exclude="")))
 
 data2map<-merge(APdata,coorddep,by="dep_ID")
 
@@ -93,14 +103,16 @@ draw.pie(x=data2map$longitude,y=data2map$latitude,
          z=cbind((as.numeric(as.character(data2map$Resistant))),
                  (as.numeric(as.character(data2map$Sensible)))),
          col=c("red","blue"),
-         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),labels=NA)
+         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),
+         labels=NA)
 par(op)
 
 #producing the barplot
 datXyear<-t(prop.table(table(ven_germ$year,ven_germ$AP,exclude=""),
                        margin=1)*100)
 totalyear<-rowSums(table(ven_germ$year,ven_germ$AP,exclude=""))
-temp<-barplot(datXyear,col=c("red","blue"),las=1,font=2,main="AP",cex.main=2)
+temp<-barplot(datXyear,col=c("red","blue"),las=1,font=2,main="AP",
+              cex.main=2)
 text(temp[1],103,paste("n=",totalyear[1],sep=""),font=3,cex=1,xpd=TRUE)
 text(temp[2],103,paste("n=",totalyear[2],sep=""),font=3,cex=1,xpd=TRUE)
 text(temp[3],103,paste("n=",totalyear[3],sep=""),font=3,cex=1,xpd=TRUE)
@@ -119,10 +131,14 @@ text(temp[11],103,paste("n=",totalyear[11],sep=""),font=3,cex=1,xpd=TRUE)
 ###############################################################################
 
 #producing the map
-CAPdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,ven_germ$captane,exclude="")),
-              "Resistant"=table(ven_germ$dptmt,ven_germ$captane,exclude="")[,1],
-              "Sensible"=table(ven_germ$dptmt,ven_germ$captane,exclude="")[,2],
-              "Total"=rowSums(table(ven_germ$dptmt,ven_germ$captane,exclude="")))
+CAPdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,
+                                        ven_germ$captane,exclude="")),
+              "Resistant"=table(ven_germ$dptmt,
+                                ven_germ$captane,exclude="")[,1],
+              "Sensible"=table(ven_germ$dptmt,
+                               ven_germ$captane,exclude="")[,2],
+              "Total"=rowSums(table(ven_germ$dptmt,
+                                    ven_germ$captane,exclude="")))
 
 data2map<-merge(CAPdata,coorddep,by="dep_ID")
 
@@ -132,14 +148,16 @@ draw.pie(x=data2map$longitude,y=data2map$latitude,
          z=cbind((as.numeric(as.character(data2map$Resistant))),
                   (as.numeric(as.character(data2map$Sensible)))),
          col=c("red","blue"),
-         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),labels=NA)
+         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),
+         labels=NA)
 par(op)
 
 #producing the barplot
 datXyear<-t(prop.table(table(ven_germ$year,ven_germ$captane,exclude=""),
                        margin=1)*100)
 totalyear<-rowSums(table(ven_germ$year,ven_germ$captane,exclude=""))
-temp<-barplot(datXyear,col=c("red","blue"),las=1,font=2,main="captane",cex.main=2)
+temp<-barplot(datXyear,col=c("red","blue"),las=1,font=2,main="captane",
+              cex.main=2)
 text(temp[1],103,paste("n=",totalyear[1],sep=""),font=3,cex=1,xpd=TRUE)
 text(temp[2],103,paste("n=",totalyear[2],sep=""),font=3,cex=1,xpd=TRUE)
 text(temp[3],103,paste("n=",totalyear[3],sep=""),font=3,cex=1,xpd=TRUE)
@@ -153,6 +171,184 @@ text(temp[10],103,paste("n=",totalyear[10],sep=""),font=3,cex=1,xpd=TRUE)
 text(temp[11],103,paste("n=",totalyear[11],sep=""),font=3,cex=1,xpd=TRUE)
 
 
+###############################################################################
+#dithianon figures
+###############################################################################
+
+#producing the map
+DITdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,
+                                        ven_germ$dithianon,exclude="")),
+               "Sensible"=table(ven_germ$dptmt,
+                                ven_germ$dithianon,exclude="")[,1],
+               "Total"=rowSums(table(ven_germ$dptmt,
+                                     ven_germ$dithianon,exclude="")))
+
+data2map<-merge(DITdata,coorddep,by="dep_ID")
+
+op<-par(mar=c(0,0,0,0))
+plot(departe)
+draw.pie(x=data2map$longitude,y=data2map$latitude,
+         z=cbind((as.numeric(as.character(data2map$Sensible))),
+                 (as.numeric(as.character(data2map$Sensible)))),
+         col=c("blue"),
+         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),
+         labels=NA)
+par(op)
+
+#producing the barplot
+datXyear<-t(prop.table(table(ven_germ$year,ven_germ$dithianon,exclude=""),
+                       margin=1)*100)
+totalyear<-rowSums(table(ven_germ$year,ven_germ$dithianon,exclude=""))
+temp<-barplot(datXyear,col=c("blue"),las=1,font=2,main="dithianon",
+              cex.main=2)
+text(temp[1],103,paste("n=",totalyear[1],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[2],103,paste("n=",totalyear[2],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[3],103,paste("n=",totalyear[3],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[4],103,paste("n=",totalyear[4],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[5],103,paste("n=",totalyear[5],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[6],103,paste("n=",totalyear[6],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[7],103,paste("n=",totalyear[7],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[8],103,paste("n=",totalyear[8],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[9],103,paste("n=",totalyear[9],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[10],103,paste("n=",totalyear[10],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[11],103,paste("n=",totalyear[11],sep=""),font=3,cex=1,xpd=TRUE)
 
 
+###############################################################################
+#boscalid figures
+###############################################################################
 
+#producing the map
+BOSdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,
+                                        ven_germ$boscalid,exclude="")),
+               "Resistant"=table(ven_germ$dptmt,
+                                 ven_germ$boscalid,exclude="")[,1],
+               "Sensible"=table(ven_germ$dptmt,
+                                ven_germ$boscalid,exclude="")[,2],
+               "Total"=rowSums(table(ven_germ$dptmt,
+                                     ven_germ$boscalid,exclude="")))
+
+data2map<-merge(BOSdata,coorddep,by="dep_ID")
+
+op<-par(mar=c(0,0,0,0))
+plot(departe)
+draw.pie(x=data2map$longitude,y=data2map$latitude,
+         z=cbind((as.numeric(as.character(data2map$Resistant))),
+                 (as.numeric(as.character(data2map$Sensible)))),
+         col=c("red","blue"),
+         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),
+         labels=NA)
+par(op)
+
+#producing the barplot
+datXyear<-t(prop.table(table(ven_germ$year,ven_germ$boscalid,exclude=""),
+                       margin=1)*100)
+totalyear<-rowSums(table(ven_germ$year,ven_germ$boscalid,exclude=""))
+temp<-barplot(datXyear,col=c("red","blue"),las=1,font=2,main="boscalid",
+              cex.main=2)
+text(temp[1],103,paste("n=",totalyear[1],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[2],103,paste("n=",totalyear[2],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[3],103,paste("n=",totalyear[3],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[4],103,paste("n=",totalyear[4],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[5],103,paste("n=",totalyear[5],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[6],103,paste("n=",totalyear[6],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[7],103,paste("n=",totalyear[7],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[8],103,paste("n=",totalyear[8],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[9],103,paste("n=",totalyear[9],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[10],103,paste("n=",totalyear[10],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[11],103,paste("n=",totalyear[11],sep=""),font=3,cex=1,xpd=TRUE)
+
+
+###############################################################################
+#dodine figures
+###############################################################################
+
+#producing the map
+DODdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,
+                                        ven_germ$dodine,exclude="")),
+               "Resistant"=table(ven_germ$dptmt,
+                                 ven_germ$dodine,exclude="")[,1],
+               "Sensible"=table(ven_germ$dptmt,
+                                ven_germ$dodine,exclude="")[,2],
+               "Total"=rowSums(table(ven_germ$dptmt,
+                                     ven_germ$dodine,exclude="")))
+
+data2map<-merge(DODdata,coorddep,by="dep_ID")
+
+op<-par(mar=c(0,0,0,0))
+plot(departe)
+draw.pie(x=data2map$longitude,y=data2map$latitude,
+         z=cbind((as.numeric(as.character(data2map$Resistant))),
+                 (as.numeric(as.character(data2map$Sensible)))),
+         col=c("red","blue"),
+         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),
+         labels=NA)
+par(op)
+
+#producing the barplot
+datXyear<-t(prop.table(table(ven_germ$year,ven_germ$dodine,exclude=""),
+                       margin=1)*100)
+totalyear<-rowSums(table(ven_germ$year,ven_germ$dodine,exclude=""))
+temp<-barplot(datXyear,col=c("red","blue"),las=1,font=2,main="dodine",
+              cex.main=2)
+text(temp[1],103,paste("n=",totalyear[1],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[2],103,paste("n=",totalyear[2],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[3],103,paste("n=",totalyear[3],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[4],103,paste("n=",totalyear[4],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[5],103,paste("n=",totalyear[5],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[6],103,paste("n=",totalyear[6],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[7],103,paste("n=",totalyear[7],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[8],103,paste("n=",totalyear[8],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[9],103,paste("n=",totalyear[9],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[10],103,paste("n=",totalyear[10],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[11],103,paste("n=",totalyear[11],sep=""),font=3,cex=1,xpd=TRUE)
+
+
+###############################################################################
+#tebuconazole figures
+###############################################################################
+
+#producing the map
+TEBdata<-cbind("dep_ID"=row.names(table(ven_germ$dptmt,
+                                        ven_germ$tebuconazole,exclude="")),
+               "Resistant"=table(ven_germ$dptmt,
+                                 ven_germ$tebuconazole,exclude="")[,1],
+               "Sensible"=table(ven_germ$dptmt,
+                                ven_germ$tebuconazole,exclude="")[,2],
+               "Total"=rowSums(table(ven_germ$dptmt,
+                                     ven_germ$tebuconazole,exclude="")))
+
+data2map<-merge(TEBdata,coorddep,by="dep_ID")
+
+op<-par(mar=c(0,0,0,0))
+plot(departe)
+draw.pie(x=data2map$longitude,y=data2map$latitude,
+         z=cbind((as.numeric(as.character(data2map$Resistant))),
+                 (as.numeric(as.character(data2map$Sensible)))),
+         col=c("red","blue"),
+         radius=(sqrt(as.numeric(as.character(data2map$Total)))*8000),
+         labels=NA)
+par(op)
+
+#producing the barplot
+datXyear<-t(prop.table(table(ven_germ$year,ven_germ$tebuconazole,exclude=""),
+                       margin=1)*100)
+totalyear<-rowSums(table(ven_germ$year,ven_germ$tebuconazole,exclude=""))
+temp<-barplot(datXyear,col=c("red","blue"),las=1,font=2,main="tebuconazole",
+              cex.main=2)
+text(temp[1],103,paste("n=",totalyear[1],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[2],103,paste("n=",totalyear[2],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[3],103,paste("n=",totalyear[3],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[4],103,paste("n=",totalyear[4],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[5],103,paste("n=",totalyear[5],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[6],103,paste("n=",totalyear[6],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[7],103,paste("n=",totalyear[7],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[8],103,paste("n=",totalyear[8],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[9],103,paste("n=",totalyear[9],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[10],103,paste("n=",totalyear[10],sep=""),font=3,cex=1,xpd=TRUE)
+text(temp[11],103,paste("n=",totalyear[11],sep=""),font=3,cex=1,xpd=TRUE)
+
+
+###############################################################################
+#END
+###############################################################################
